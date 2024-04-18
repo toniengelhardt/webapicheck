17ab591c40bbd9deed68faab39fc77ad4ad0cead
Toni <2703233+toniengelhardt@users.noreply.github.com>
2024-04-18 13:24:27 +0100

    fix: add happy-dom

--- server/api/__sitemap__/urls.ts
+++ server/api/__sitemap__/urls.ts
@@ -1,8 +1,7 @@
 // @ts-expect-error 2305
 import { asSitemapUrl, defineSitemapEventHandler } from '#imports'
-
 import { webApiExportList } from '@/utils/export'
 
-export default defineSitemapEventHandler(async () => {
-  return webApiExportList.map(webApiId => asSitemapUrl({ loc: `/apis/${webApiId}` }))
-})
+export default defineSitemapEventHandler(async () => (
+  webApiExportList.map(webApiId => asSitemapUrl({ loc: `/apis/${webApiId}` }))
+))
